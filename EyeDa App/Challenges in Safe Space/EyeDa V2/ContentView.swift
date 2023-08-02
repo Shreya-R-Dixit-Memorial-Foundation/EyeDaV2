5e33c74eaab516d02ac8330d368c940282ece5c7
Pranika Kumar <kumar869@umn.edu>
2023-08-02 07:06:48 +0530

    outline

---
+++ EyeDa App/Challenges in Safe Space/EyeDa V2/ContentView.swift
@@ -0,0 +1,80 @@
+
+import SwiftUI
+
+extension UIColor {
+    convenience init(hex: String) {
+        var hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if hexString.hasPrefix("#") {
+            hexString.remove(at: hexString.startIndex)
+        }
+
+        if hexString.count != 6 {
+            self.init(white: 0.5, alpha: 1.0)
+            return
+        }
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexString).scanHexInt64(&rgbValue)
+
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgbValue & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+}
+
+struct TabBarItem: View {
+    let icon: String
+    let label: String
+    let iconColor: Color
+
+    init(icon: String, label: String, iconColorHex: String) {
+        self.icon = icon
+        self.label = label
+        self.iconColor = Color(UIColor(hex: iconColorHex))
+    }
+
+    var body: some View {
+        VStack {
+            Image(systemName: icon)
+                .foregroundColor(iconColor)
+            Text(label)
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            NavigationView {
+                HomeView()
+            }
+            .tabItem {
+                TabBarItem(icon: "house.fill", label: "Home", iconColorHex: "#1A5944")
+            }
+            
+            NavigationView {
+                SettingsView()
+            }
+            .tabItem {
+                TabBarItem(icon: "gearshape.fill", label: "Settings", iconColorHex: "#1A5944")
+            }
+            
+            NavigationView {
+                SquadsView()
+            }
+            .tabItem {
+                TabBarItem(icon: "person.2.fill", label: "Squads", iconColorHex: "#1A5944")
+            }
+            
+            NavigationView {
+                EyeView()
+            }
+            .tabItem {
+                TabBarItem(icon: "eye.fill", label: "Eyeda", iconColorHex: "#1A5944")
+            }
+        }
+    }
+}
---
+++ EyeDa App/Challenges in Safe Space/EyeDa V2/EyeDa V2/EyeDa_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  EyeDa_V2App.swift
+//  EyeDa V2
+//
+//  Created by Pranika Kumar on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct EyeDa_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
